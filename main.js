4e51f453cc4fe4d6edc6b34c25714272919a324e
Robertson, Dalton <daltoncrobertson@gmail.com>
2017-05-16 19:33:17 -0500

    animation for ball

---
+++ main.js
@@ -0,0 +1,29 @@
+import Expo from 'expo';
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+import Ball from './src/ball';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Ball />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    flex: 1,
+    backgroundColor: '#fff'
+  }
+});
+
+Expo.registerRootComponent(App);
